dc3350b3d6ac0a5021845f2edb187d13b766ceff
Chris-Keo <93360976+Chris-Keo@users.noreply.github.com>
2023-08-31 00:10:13 -0400

    page 14

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,18 @@ from app import app
 @app.route('/')        # these are decorators, a decorator modifies the functions that follows it
 @app.route('/index')   # a common pattern w/ decorators is to use them to register funcs as callbacks for certain events
 def index():
-    user = {'username': 'Bora'}
-    return render_template('index.html', title='Home', user=user)
+    user = {'username': 'Harry'}
+    posts = [
+        {
+        'author':{'username':'John'},
+        'body': 'Beatiful day in Portland!'
+        },
+        {
+        'author':{'username':'Susan'},
+        'body': 'Gran Turismo is an underdog movie. Recommmend!'
+        }
+     
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
 
