f17a8985fc155be2cffc894b092e72987561a067
Chris-Keo <93360976+Chris-Keo@users.noreply.github.com>
2023-11-20 18:36:32 -0500

    onto chapther 4

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,14 @@
+"""
+Flask-WTF uses Python classes to represent web forms.
+A form class defines the fields of the forms as class variables.
+"""
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password =  PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()]) # since there is a validators attached, this means flask will generate errors to render in html 
+    password =  PasswordField('Password', validators=[DataRequired()]) 
     remember_me = BooleanField('Remember Me')
     submit =SubmitField('Sign In')
--- app/routes.py
+++ app/routes.py
@@ -8,8 +8,9 @@ View Functions
 
 Each View Function can be mapped to one or more URLs
 """
-from flask import render_template # this method converts a template into an HTML page. This invokes Jinja2 template engine. Its shipped with Flask
+from flask import render_template, flash, redirect, url_for# this method converts a template into an HTML page. This invokes Jinja2 template engine. Its shipped with Flask
 from app import app # this references the app folder and the app instance inside __init__.py
+from app.forms import LoginForm
 
 # Above each function are URL routes
     # these are decorators, a decorator modifies the functions that follows it
@@ -28,6 +29,15 @@ def index():
         'body': 'Gran Turismo is an underdog movie. Recommmend!'
         }   
         ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title = 'Home', user = user, posts = posts)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm() # Instantiate an object from forms
+    if form.validate_on_submit(): # This will evaluate to True if the user clicks the submit button to POST data. 
+                                  # Then it will look at forms > LoginForms to validate the data class. If all valid then eval to True.
+                                    # So if the user hits triggers the POST method and the data is valid, then it will use flash, which is a method to show user its good
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect('/index') # redirects user to another page
+    return render_template('login.html', title = 'Sign In', form = form)
 
